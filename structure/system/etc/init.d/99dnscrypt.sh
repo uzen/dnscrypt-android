09c67c9f54d04f6c06e307ab40903f02a2358c69
Dmitry <uzensqr@gmail.com>
2018-09-27 04:39:52 +0500

    simplify the function call of the rules update

--- structure/system/etc/init.d/99dnscrypt.sh
+++ structure/system/etc/init.d/99dnscrypt.sh
@@ -15,8 +15,6 @@ DESC="dns client proxy"
 
 . /system/etc/dnscrypt-proxy/init-functions
 
-WAITFORDAEMON=30
-
 # Exit if the package is not installed
 test -x $DAEMON || exit 0
 
@@ -34,86 +32,86 @@ log_error_msg () {
 	fi
 }
 
-get_prop () { getpropf $LOCKFILE "$@"; }
-set_prop () { setpropf $LOCKFILE "$1" "$2"; }
+get_prop () { getpropf "$LOCKFILE" "$@"; }
+set_prop () { setpropf "$LOCKFILE" "$1" "$2"; }
 
-check_health () {           
-   test -s "$LOCKFILE" || return 1
-   
-	if [ "$(get_prop 'dnscrypt-resolvers')" = "none" ]; then
-		confdir=`dirname "$CONFIG_FILE"`
-		intdir="/sdcard/$NAME"
-		
-		resolvers="public-resolvers.md"
-		minisig="$resolvers.minisig"
-		
-		if check_resolvers $confdir/$resolvers; then
-			log_debug_msg "copy $confdir/$resolvers to $PIDDIR..."
-			cp $confdir/{$resolvers,$minisig} $PIDDIR/
-		elif check_resolvers $intdir/$resolvers; then
-			log_debug_msg "copy $intdir/$resolvers to $PIDDIR..."
-			cp $intdir/{$resolvers,$minisig} $PIDDIR/
-		else
-			log_debug_msg "$resolvers(.minisig): file not found"
-		fi
-   fi
+check_health () {
+    if [ -s "$LOCKFILE" ]; then
+        if [ "$(get_prop 'dnscrypt-resolvers')" = "none" ]; then         
+            resolvers="public-resolvers.md"
+            minisig="$resolvers.minisig"
+				
+            confdir=${DNSCRYPT_RESOLV_PATH:-`dirname "$CONFIG_FILE"`}
             
-   if [ "$(get_prop 'ipv4-enabled')" = "false" ]; then
-      log_debug_msg "ipv4_addr_unlock: enable IPv4"
-      ipv4_addr_unlock
-   fi
-   
-   log_debug_msg "$LOCKFILE file has been removed"     
-   rm -f "$LOCKFILE"
+         	if check_resolvers $confdir/$resolvers; then
+					log_debug_msg "copy $confdir/$resolvers to $PIDDIR..."
+					cp $confdir/{$resolvers,$minisig} $PIDDIR/
+				else
+					log_debug_msg "$confdir/$resolvers(.minisig): file not found"
+            fi
+        fi
+        
+        if [ "$(get_prop 'ipv4-enabled')" = "false" ]; then
+            log_debug_msg "ipv4_addr_unlock: enable IPv4"
+            ipv4_addr_unlock
+        fi
+        	   
+        log_debug_msg "$LOCKFILE file has been removed"     
+        rm -f "$LOCKFILE"
+    fi
 }
 
 _wfd_call () {
-	[ -e $PIDDIR/*.md ] || return 1 
+	if ! ls "$PIDDIR"/*.md 2>/dev/null; the
+		return 1
+	fi
 }
 
 do_start () {
     if test ! -s "$CONFIG_FILE"; then
         log_debug_msg "missing config file $CONFIG_FILE"
-        exit 0
+        exit 1
     fi
      
     mkdir -p -m 01755 "$PIDDIR" 2>/dev/null || \
         { log_debug_msg "cannot access $PIDDIR directory, are you root?"; exit 1; }
 
-    nohup $DAEMON -config $CONFIG_FILE \
-         -pidfile=$PIDFILE > /dev/null 2>&1 &
-    RETVAL=$! && printf "$RETVAL\n" > $PIDFILE
+    nohup $DAEMON -config "$CONFIG_FILE" \
+         -pidfile="$PIDFILE" > /dev/null 2>&1 &
+    PIDVAL=$! && echo "$PIDVAL" > "$PIDFILE"
     
     status="0"
     status_of_proc "$DAEMON" "$PIDFILE" || status="$?"
-    
+
     case "$status" in
-       0) # ok
-       		wait_for_daemon _wfd_call || status="$?"
-            log_debug_msg "enabling iptables firewall rules"
-            do_iptables_rules 0
-            if [ "$status" = 1 ]; then
+       0)
+            if ! wait_for_daemon _wfd_call; then
                 log_error_msg "the resolvers file couldn't be uploaded?"
                 set_prop "dnscrypt-resolvers" "none"
                 return 10
             fi
+            log_debug_msg "enabling iptables firewall rules"
+            iptrules_on
             ;;
-       *) # offline	
+       *) # offline
             log_error_msg "ipv4_addr_lock: disable IPv4 (#$status)"
             ipv4_addr_lock && $(set_prop "ipv4-enabled" "false")
             return 1
             ;;
     esac
+    return 0
 }
 
 do_stop () {    
-    if killproc "$DAEMON" "$PIDFILE"; then
-        log_debug_msg "disabling iptables firewall rules"
-        do_iptables_rules 1
-    else
+    if ! killproc "$DAEMON" "$PIDFILE"; then
         log_debug_msg "$DAEMON died: process not running or permission denied"
         exit 1
     fi
+    
+    log_debug_msg "disabling iptables firewall rules"
+    iptrules_off
+    
+    check_health
     
     if [ -f "$PIDFILE" ]; then
         status_of_proc "$DAEMON" "$PIDFILE" || rm -f "$PIDFILE"
@@ -129,9 +127,28 @@ do_restart () {
 case "$1" in
   start)
         log_debug_msg "starting $DESC"
-  
-        if ! do_start; then
-            check_health
+        prev=""
+        for arg in "$@"; do
+            if [[ $arg == $1 ]]; then
+                continue
+            elif [[ $arg == -f || $arg == --force ]]; then
+                DNSCRYPT_FORCE=1
+            elif [[ $arg == -r || $arg == --resolv_path ]]; then
+                :
+            elif [[ $prev == -r || $prev == --resolv_path ]]; then
+                DNSCRYPT_RESOLV_PATH=$arg
+            else
+                echo Unrecognized argument $arg
+                exit 2
+            fi
+            prev=$arg
+        done
+        
+        status="0"
+        do_start || status="$?"
+        
+        if [[ "$status" -ne 0 && "$DNSCRYPT_FORCE" = 1 ]]; then
+            log_debug_msg "restart $DESC"
             do_restart
         fi
         ;;
@@ -156,7 +173,7 @@ case "$1" in
   *)
         echo "Usage: /etc/init.d/99dnscrypt {start|stop|restart|status}" >&2
         exit 1
-  ;;
+        ;;
 esac
 
 exit 0
